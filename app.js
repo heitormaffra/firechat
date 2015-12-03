152439fc7141469515fee0e0e779a68dedc39cbe
João Lucas <john_lucas_666@hotmail.com>
2015-12-03 17:29:30 -0200

    create app.js
    including some jQuery code to handle page's events

---
+++ app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  var myDataRef = new Firebase('https://i1cxkjpynl8.firebaseio-demo.com/');
+  
+  myDataRef.on('child_added', function(snapshot) {
+    var message = snapshot.val();    
+    var talk = "<span id=\"talk\"><strong>" + message.name + ": </strong>" + message.text + "</span>";
+    $('.chat-panel').append(talk);    
+  });
+  
+  $('#name').keypress(function(e){
+    if (e.keyCode == 13) {      
+      $(this).prop('disabled', true);
+    }
+  });
+  
+  $('#clear').click(function(){    
+    $('#name').prop('disabled', false);
+    $('#name').val('');
+  });
+    
+  $('#okButton').click(function(){
+    var name = $('#name').val();
+    var text = $('#message').val();
+    
+    if(name != "" && text != "")
+    {
+      myDataRef.push({name: name, text: text});
+      $('#message').val('');
+    }
+  });
+});
